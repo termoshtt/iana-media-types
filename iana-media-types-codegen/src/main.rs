e701418e9c512d6cbf1546303eba6c7596885112
Toshiki Teramura <toshiki.teramura@gmail.com>
2022-11-14 18:52:05 +0900

    Generate enum definition

--- iana-media-types-codegen/src/main.rs
+++ iana-media-types-codegen/src/main.rs
@@ -1,6 +1,50 @@
 use anyhow::Result;
 use inflector::Inflector;
 
+#[derive(Debug, Clone)]
+struct MediaType {
+    name: syn::Ident,
+    member_idents: Vec<syn::Ident>,
+    member_templates: Vec<String>,
+}
+
+impl MediaType {
+    fn new(name: syn::Ident, url: &str) -> Result<Self> {
+        let csv = ureq::get(url).call()?.into_string()?;
+        let mut member_idents = Vec::new();
+        let mut member_templates = Vec::new();
+        for line in csv
+            .lines()
+            .skip(1 /* CSV header */)
+            .filter(|line| !line.contains("OBSOLETED"))
+        {
+            let mut iter = line.split(',');
+            match (iter.next(), iter.next()) {
+                (Some(name), Some(media_type)) => {
+                    member_idents.push(as_ident(name));
+                    member_templates.push(media_type.trim().to_string());
+                }
+                _ => continue,
+            }
+        }
+        Ok(Self {
+            name,
+            member_idents,
+            member_templates,
+        })
+    }
+
+    fn enum_definition(&self) -> proc_macro2::TokenStream {
+        let name = &self.name;
+        let member_idents = &self.member_idents;
+        quote::quote! {
+            pub enum #name {
+                #(#member_idents),*
+            }
+        }
+    }
+}
+
 fn as_ident(name: &str) -> syn::Ident {
     let name = name.replace(['.', '+'], "-").to_class_case();
     if name.starts_with(|c: char| c.is_digit(10)) {
@@ -10,28 +54,11 @@ fn as_ident(name: &str) -> syn::Ident {
     }
 }
 
-fn get_media_type_from_url(url: &str) -> Result<Vec<(syn::Ident, String)>> {
-    let csv = ureq::get(url).call()?.into_string()?;
-    Ok(csv
-        .lines()
-        .filter_map(|line| {
-            if line.contains("OBSOLETED") {
-                return None;
-            }
-            let mut iter = line.split(',');
-            match (iter.next(), iter.next()) {
-                (Some(name), Some(media_type)) => {
-                    Some((as_ident(name.trim()), media_type.trim().to_string()))
-                }
-                _ => None,
-            }
-        })
-        .collect())
-}
-
 fn main() -> Result<()> {
-    let application =
-        get_media_type_from_url("https://www.iana.org/assignments/media-types/application.csv")?;
-    dbg!(application);
+    let application = MediaType::new(
+        quote::format_ident!("MediaType"),
+        "https://www.iana.org/assignments/media-types/application.csv",
+    )?;
+    println!("{}", application.enum_definition());
     Ok(())
 }
